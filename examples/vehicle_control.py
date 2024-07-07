52a4aca0f2e5874d7e4bbbdb9c8d961b5eacffa3
kosuke55 <kosuke.tnp@gmail.com>
2024-07-07 14:36:06 +0900

    feat: add overtaking visualization
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- examples/vehicle_control.py
+++ examples/vehicle_control.py
@@ -66,3 +66,78 @@ x0 = np.array([0, 1.5, 0, 1, 0,
 us_init = np.random.randn(N, n_u)*0.0001
 #get optimal states and actions
 xs, us, cost_trace = controller.fit(x0, us_init, 100)
+
+#visualize the overtaking scenario
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.animation import FuncAnimation
+from matplotlib.transforms import Affine2D
+import numpy as np
+
+def visualize(xs):
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    ax.set_xlim(0, 30)
+    ax.set_ylim(-3.1, 3.1)
+    ax.set_aspect('equal')
+    ax.axis("off")
+
+    for boundary_y in [-3, 3]:
+        ax.plot([0, 30], [boundary_y, boundary_y], 'k-', linewidth=1.0)
+
+    for lane_y in [0, 1.5]:
+        ax.plot([0, 30], [lane_y, lane_y], 'k--', linewidth=1.0)
+
+    ego_length = 2.0
+    ego_width = 1.0
+    other_length = 2.0
+    other_width = 1.0
+
+    ego_rect = patches.Rectangle((0, 0), ego_length, ego_width, fc='r', ec='r', alpha=0.5)
+    other_rect = patches.Rectangle((0, 0), other_length, other_width, fc='g', ec='g', alpha=0.5)
+    ax.add_patch(ego_rect)
+    ax.add_patch(other_rect)
+
+    ego_trajectory, = ax.plot([], [], 'r-', label='Ego vehicle trajectory')
+    other_trajectory, = ax.plot([], [], 'g-', label='Other vehicle trajectory')
+
+    def init():
+        ego_rect.set_xy((xs[0, 0] - ego_length / 2, xs[0, 1] - ego_width / 2))
+        ego_rect.angle = np.degrees(xs[0, 2])
+        other_rect.set_xy((xs[0, 5] - other_length / 2, xs[0, 6] - other_width / 2))
+        other_rect.angle = np.degrees(xs[0, 7])
+        ego_trajectory.set_data([], [])
+        other_trajectory.set_data([], [])
+        return ego_rect, other_rect, ego_trajectory, other_trajectory
+
+    def update(frame):
+        ego_center_x = xs[frame, 0]
+        ego_center_y = xs[frame, 1]
+        ego_angle = np.degrees(xs[frame, 2])
+
+        other_center_x = xs[frame, 5]
+        other_center_y = xs[frame, 6]
+        other_angle = np.degrees(xs[frame, 7])
+
+        ego_transform = Affine2D().rotate_deg_around(ego_center_x, ego_center_y, ego_angle) + ax.transData
+        ego_rect.set_transform(ego_transform)
+        ego_rect.set_xy((ego_center_x - ego_length / 2, ego_center_y - ego_width / 2))
+
+        other_transform = Affine2D().rotate_deg_around(other_center_x, other_center_y, other_angle) + ax.transData
+        other_rect.set_transform(other_transform)
+        other_rect.set_xy((other_center_x - other_length / 2, other_center_y - other_width / 2))
+
+        ego_trajectory.set_data(xs[:frame+1, 0], xs[:frame+1, 1])
+        other_trajectory.set_data(xs[:frame+1, 5], xs[:frame+1, 6])
+        return ego_rect, other_rect, ego_trajectory, other_trajectory
+
+    ani = FuncAnimation(fig, update, frames=range(len(xs)), init_func=init, blit=True, interval=50)
+
+    plt.xlabel('X position')
+    plt.ylabel('Y position')
+    plt.title('Vehicle Overtaking Visualization with Trajectories')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+visualize(xs)
